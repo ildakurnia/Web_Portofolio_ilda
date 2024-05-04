01484b6be60c083bea795c75bf8215efc15734d3
ilda kurnia <ildakurnia@gmail.com>
2024-05-04 21:42:33 +0700

    update contact

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -20,7 +20,7 @@ export const Contact = () => {
             src={getImageUrl("contact/linkedinIcon.png")}
             alt="LinkedIn icon"
           />
-          <a href="https:// www.linkedin.com/in/ildakurnia">linkedin.com/ilda kurnia</a>
+          <a href="https://www.linkedin.com/in/ildakurnia" target="blank">linkedin.com/ilda kurnia</a>
         </li>
         <li className={styles.link}>
           <img src={getImageUrl("contact/githubIcon.png")} alt="Github icon" />
